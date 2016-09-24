8e59e90d96e73560c9c4a041332ece5465b0ae5d
mohumohu <b1621ry@aiit.ac.jp>
2016-09-24 15:35:19 +0900

    make index.html

--- hello.rb
+++ hello.rb
@@ -9,7 +9,16 @@ get '/' do
     c.application_id = ENV["APPID"]
     c.affiliate_id = ENV["AFID"]
   end
+  # Use genre id to fetch genre object
+  @rankings = RakutenWebService::Ichiba::Item.ranking(:age => 40, :sex => 0)
+  erb :item_ranking
+end
 
+get '/books/' do
+  RakutenWebService.configuration do |c|
+    c.application_id = ENV["APPID"]
+    c.affiliate_id = ENV["AFID"]
+  end
   # Use genre id to fetch genre object
   @rankings = RakutenWebService::Ichiba::Item.ranking(:age => 40, :sex => 0)
   erb :item_ranking
